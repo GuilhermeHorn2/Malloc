5d834106887b12a0eb7f6bf903a476916b128d63
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-06-18 09:06:20 -0300

    Create code.c

---
+++ code.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+void* align_malloc(void *ptr,int size,int n);
+
+void aligned_free(void* ptr);
+
+int main()
+{
+ /* ?\n;||(or) e &&(and)*/
+
+
+
+	char* ptr = (char*)align_malloc(ptr,1000,4);
+	if((int)ptr % 4 == 0){
+		printf("%d\n",ptr);
+	}
+	aligned_free(ptr);
+
+ return 0;
+}
+
+
+void* align_malloc(void *ptr,int size,int n){
+
+
+	while(1){
+		ptr = malloc(sizeof(void) * size);
+		if((int)ptr % n != 0){
+			aligned_free(ptr);
+			continue;
+		}
+		break;
+	}
+	return ptr;
+}
+
+
+void aligned_free(void *ptr){
+	free(ptr);
+}
